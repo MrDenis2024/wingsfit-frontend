52f12dd1a1ab4ee22e07907916a4312d427f547f
alyonaVet <166475051+alyonaVet@users.noreply.github.com>
2024-10-28 16:22:23 +0600

    Создание фронта (#1)

---
+++ src/app/store.ts
@@ -0,0 +1,35 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+
+// const usersPersistConfig = {
+//   key: "wingsfit:users",
+//   storage,
+//   whitelist: ["user"],
+// };
+
+const rootReducer = combineReducers({
+  // users: persistReducer(usersPersistConfig, usersReducer),
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
---
+++ src/axiosApi.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { apiURL } from "./constants.ts";
+import { Store } from "redux";
+import { RootState } from "./app/store.ts";
+
+const axiosApi = axios.create({
+  baseURL: apiURL,
+});
+
+export const addInterceptors = (store: Store<RootState>) => {
+  axiosApi.interceptors.request.use((request) => {
+    const token = store.getState().users.user?.token;
+    if (token) {
+      request.headers.set("Authorization", `Bearer ${token}`);
+    }
+    return request;
+  });
+};
+
+export default axiosApi;
