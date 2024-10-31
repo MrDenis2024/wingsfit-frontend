763c3e38d08b0d1ca056b0cd73cb1234d90ee9b9
DarkFlarex <darkflarexxx@gmail.com>
2024-10-31 14:47:06 +0600

    Создал карточки тренер и рассписания. Вывел через home. Добавил картинку заглушки.

---
+++ src/trainer/components/TrainerCard.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {Card, CardContent, CardHeader, CardMedia, Grid2, styled} from "@mui/material";
+import {Link} from "react-router-dom";
+import imageNotFound from '/src/assets/images/user-icon-not-found.png';
+import {apiURL} from "../../constants.ts";
+
+const ImageCardMedia = styled(CardMedia)({
+    width: '50%',
+    height: 0,
+    paddingTop: '56.25%',
+    borderRadius: '10px',
+    backgroundColor: 'silver',
+});
+
+const StyledLink = styled(Link)({
+    color: 'inherit',
+    textDecoration: 'none',
+});
+
+interface Props {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    avatar: string | null;
+    experience: string;
+}
+
+const TrainerCard: React.FC<Props> = ({_id, firstName, lastName, avatar, experience}) => {
+    let cardImage = imageNotFound;
+
+    if (avatar) {
+        cardImage = `${apiURL}/${avatar}`;
+    }
+
+    return (
+        <>
+            <Grid2 sx={{
+                width: {
+                    xs: '150px',
+                    sm: '270px',
+                },
+            }}>
+                <StyledLink to={`/trainer/${_id}`}>
+                    <Card sx={{
+                        height: '100%',
+                        paddingTop: '24px',
+                        backgroundColor: '#f0f0f0',
+                    }}>
+                        <Grid2 display="flex" justifyContent="center">
+                            <ImageCardMedia image={cardImage} title={`${firstName} ${lastName}`} />
+                        </Grid2>
+                        <CardHeader title={`${firstName} ${lastName}`} />
+                        <CardContent sx={{ paddingBottom: '0' }}>
+                            {experience}
+                        </CardContent>
+                    </Card>
+                </StyledLink>
+            </Grid2>
+        </>
+
+    );
+};
+
+export default TrainerCard;
