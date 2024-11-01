0e3dce027d49df4c3d6ff64815926f2789e88d13
alyonaVet <166475051+alyonaVet@users.noreply.github.com>
2024-11-01 12:24:37 +0600

    добавление формы регистрации (#5)
    * добавлен компонент Auth с регистрацией маршрутов на тренере и клиенте;
    
    * рефакторинг: добавлен компонент Auth на главную страницу;
    
    * рефакторинг: изменен часовой пояс; добавлены свойства changeTimezone, name;
    
    * добавлены типы в трейнер;
    
    * добавленные типы для клиента;
    
    * создана форма регистрации с полями для регистрации тренера или клиента;
    
    * добавлен компонент RegisterForm с навигационным Stepper;

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,21 +1,7 @@
-import { Button, Grid2 } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import Auth from "../auth/Auth";
 
 const Home = () => {
-  return (
-    <>
-      <Grid2>
-        <Grid2 display="flex" justifyContent="center" gap={2}>
-          <NavLink to="/register/treiner">
-            <Button variant="contained">Want to be a coach</Button>
-          </NavLink>
-          <NavLink to="/register/client">
-            <Button variant="contained">Want to work out</Button>
-          </NavLink>
-        </Grid2>
-      </Grid2>
-    </>
-  );
+    return <Auth/>;
 };
 
 export default Home;
