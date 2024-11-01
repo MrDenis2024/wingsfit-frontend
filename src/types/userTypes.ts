b471062fb7e7f6cf89d8a23b6ba433578ac291a4
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-11-02 00:28:33 +0600

    Изменение страницы регистрации пользователя

--- src/types/userTypes.ts
+++ src/types/userTypes.ts
@@ -8,3 +8,21 @@ export interface IUser {
   role: string;
   token: string;
 }
+
+export interface UserMutation {
+  username: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export interface ValidationError {
+  errors: {
+    [key: string]: {
+      name: string;
+      message: string;
+    };
+  };
+  message: string;
+  name: string;
+  _name: string;
+}
