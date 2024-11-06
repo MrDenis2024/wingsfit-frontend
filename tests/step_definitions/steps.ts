0f222fd9c2e2757cd5eaefc14a8f929e345bb48b
Toktobekov Aidar <aidar.omurbekov05@gmail.com>
2024-11-06 14:34:04 +0600

    Сделаны тесты для клинта и тренера

--- tests/step_definitions/steps.ts
+++ tests/step_definitions/steps.ts
@@ -1,18 +1,27 @@
 const { I } = inject();
 
-Given('я нахожусь на странице входа', () => {
-  I.amOnPage('/login/client');
+const pageMap: Record<string, string> = {
+  'входа как клиент': '/login/client',
+  'входа как тренер': '/login/trainer',
+};
+
+const userPasswords = {
+  'client@fit.local': 'test',
+  'trainer@fit.local': 'test',
+}
+
+Given('я нахожусь на странице {string}', (page: string) => {
+  I.amOnPage(pageMap[page]);
 });
 
 When('ввожу в поле {string} значение {string}', (name: string, value: string) => {
   I.fillField(name, value); 
 });
 
-When('нажимаю на кнопку {string}', () => {
-  console.log('Click to button');
-  I.wait(30);
+When('нажимаю на кнопку {string}', (name: string) => {
+  I.click(`button[type='submit']`);
 });
 
-Then('я вижу сообщение {string}', () => {
-  console.log('Good work');
+Then('я вижу сообщение {string}', (message: string) => {
+  I.see(message);
 });
