dac775ca8ba580af59fd117c8031ade4ac840bd1
Ivan Limarev <ivantaketo@gmail.com>
2024-11-08 00:22:19 +0600

    Auth.tsx: Добавление стилизации начальной страницы

--- src/features/auth/Auth.tsx
+++ src/features/auth/Auth.tsx
@@ -1,15 +1,16 @@
-import { Button, Grid2, styled } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import {Box, Button, Grid2, styled, Typography} from "@mui/material";
+import {NavLink} from "react-router-dom";
+import welcomePicture from "../../assets/images/welcome-picture.jpeg";
 
-const StyledButton = styled(Button)(({ theme }) => ({
+const StyledButton = styled(Button)(({theme}) => ({
   color: "#212121",
   border: "1px solid #212121",
-  borderRadius: theme.shape.borderRadius,
+  borderRadius: "14px",
   fontSize: 12,
   fontWeight: 600,
   textTransform: "none",
   padding: `${theme.spacing(1)} ${theme.spacing(5)}`,
-  minWidth: 200,
+  minWidth: 355,
   "&:hover": {
     backgroundColor: "#fafafa",
     boxShadow: theme.shadows[2],
@@ -20,23 +21,53 @@ const StyledButton = styled(Button)(({ theme }) => ({
 const Auth = () => {
   return (
     <>
-      <Grid2 m={5}>
-        <Grid2 display="flex" justifyContent="center" flexWrap="wrap" gap={2}>
-          <NavLink to="/register/client" style={{ textDecoration: "none" }}>
+      <Grid2 flexDirection="column" container my={10} py={2} justifyContent="center" alignItems="center">
+        <Grid2
+          sx={{
+            width: 345,
+            borderBottom: "2px solid",
+            borderTop: "2px solid",
+            borderColor: "rgba(228,228,228,0.7)",
+            my: 3,
+            px: 7,
+            py: 1,
+            pb: 2,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h2" fontWeight="bold" gutterBottom>
+            WingsFit
+          </Typography>
+        </Grid2>
+        <Grid2
+          sx={{
+            width: 270,
+            my: 1,
+            py: 1,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h6" lineHeight="1.3" fontWeight="bold" textAlign="center" gutterBottom>
+            Connect with fitness trainers for group sessions
+          </Typography>
+        </Grid2>
+          <Box
+            component="img"
+            src={welcomePicture}
+            alt="Welcome"
+            sx={{ width: "auto", height: 250, mb: 2 }}
+          />
+        <Grid2 size={12} py={2} display="flex" flexDirection="column" alignItems="center"
+               justifyContent="center" flexWrap="wrap" gap={1}>
+          <NavLink to="/login/client" style={{textDecoration: "none"}}>
             <StyledButton variant="outlined">Want to work out</StyledButton>
           </NavLink>
-          <NavLink to="/register/trainer" style={{ textDecoration: "none" }}>
+          <NavLink to="/login/trainer" style={{textDecoration: "none"}}>
             <StyledButton variant="outlined">Want to be a coach</StyledButton>
-          </NavLink>
-        </Grid2>
-      </Grid2>
-      <Grid2 m={5}>
-        <Grid2 display="flex" justifyContent="center" flexWrap="wrap" gap={2}>
-          <NavLink to="/login/client" style={{ textDecoration: "none" }}>
-            <StyledButton variant="outlined">Login as client</StyledButton>
-          </NavLink>
-          <NavLink to="/login/trainer" style={{ textDecoration: "none" }}>
-            <StyledButton variant="outlined">Login as coach</StyledButton>
           </NavLink>
         </Grid2>
       </Grid2>
