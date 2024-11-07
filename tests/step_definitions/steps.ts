c9e7c2fc2cfa4e6f3addbf1ff801bd2e61f3e4f3
Amantai <Darkflarexxx@gmail.com>
2024-11-07 13:24:01 +0600

    Созданы тесты для регистрации! (#11)

--- tests/step_definitions/steps.ts
+++ tests/step_definitions/steps.ts
@@ -1,5 +1,25 @@
 const { I } = inject();
 
+Given('я нахожусь на странице регистрация клиента', () => {
+  I.amOnPage('/register/client');
+});
+
+Given('я нахожусь на странице регистрация тренера', () => {
+  I.amOnPage('/register/trainer');
+});
+
+When('ввожу в поле {string} значение {string}', (name: string, value: string) => {
+  I.fillField(name, value);
+});
+
+When('нажимаю на кнопку {string}', (name: string) => {
+  I.click(`//button[contains(., '${name}')]`);
+});
+
+Then('я вижу сообщение {string}', (message: string) => {
+  I.see(message);
+});
+
 const pageMap: Record<string, string> = {
   'входа как клиент': '/login/client',
   'входа как тренер': '/login/trainer',
@@ -15,7 +35,7 @@ Given('я нахожусь на странице {string}', (page: string) => {
 });
 
 When('ввожу в поле {string} значение {string}', (name: string, value: string) => {
-  I.fillField(name, value); 
+  I.fillField(name, value);
 });
 
 When('нажимаю на кнопку {string}', (name: string) => {
