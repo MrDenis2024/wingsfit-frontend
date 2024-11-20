43daf7659f5f4072d8a417d61da2bf85c1e89b6e
DarkFlarex <darkflarexxx@gmail.com>
2024-11-20 14:12:49 +0600

    Улучшение courseTypes.ts

--- src/types/courseTypes.ts
+++ src/types/courseTypes.ts
@@ -1,16 +1,3 @@
-export interface Course {
-  _id: string;
-  title: string;
-  courseType: string;
-  description: string;
-  format: string;
-  schedule: string;
-  scheduleLength: string;
-  price: string;
-  maxClients: string;
-  image: string | null;
-}
-
 export interface CourseMutation {
   title: string;
   courseType: string;
@@ -29,11 +16,20 @@ export interface CourseTypeFields {
   description: string | null;
 }
 
-export interface ICourse extends CourseMutation {
+export interface ICourse {
   _id: string;
   user: {
     _id: string;
     firstName: string;
     lastName: string;
-  };
+ };
+  title: string;
+  courseType: string;
+  description: string;
+  format: string;
+  schedule: string;
+  scheduleLength: string;
+  price: number;
+  maxClients: string;
+  image: string | null;
 }
