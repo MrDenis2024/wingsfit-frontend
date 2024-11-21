ad8a3adfdfd815aa19c81ace561409679ff6bf2b
alyonaVet <a.v.alenka2015@gmail.com>
2024-11-21 13:14:38 +0600

    создан компонент StyledLink для пользовательских стилизованных навигационных ссылок;

---
+++ src/UI/AppToolbar/AnonymousMenu.tsx
@@ -0,0 +1,36 @@
+import {Link, Stack, styled} from '@mui/material';
+
+export const CustomStyledLink = styled(Link)(() => ({
+  textDecoration: "none",
+  fontSize: "11px",
+  fontWeight: 600,
+  color: "#000000",
+  position: "relative",
+  "&::after": {
+    content: '""',
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    width: "100%",
+    height: "1px",
+    backgroundColor: "#ff5136",
+    transform: "scaleY(0)",
+    transformOrigin: "bottom",
+    transition: "transform 0.75s ease",
+  },
+  "&:hover::after": {
+    transform: "scaleY(1)",
+  },
+}));
+
+const AnonymousMenu = () => {
+  return (
+    <Stack direction="row" spacing={4}>
+      <CustomStyledLink href="/">Расписание</CustomStyledLink>
+      <CustomStyledLink href="/">Тренеры</CustomStyledLink>
+      <CustomStyledLink href="/">Контакты</CustomStyledLink>
+    </Stack>
+  );
+};
+
+export default AnonymousMenu;
