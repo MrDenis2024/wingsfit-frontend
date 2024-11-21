f30ba6042b119cc9bdb12514e9d159c1d9076535
alyonaVet <a.v.alenka2015@gmail.com>
2024-11-22 01:14:48 +0600

    добавлен компонент TryFreeButton;

---
+++ src/features/welcomePage/components/buttons/TryFreeButton.tsx
@@ -0,0 +1,35 @@
+import {Button, styled} from "@mui/material";
+import React from "react";
+
+const StyledButton = styled(Button)<{ textColor: string, backgroundColor: string, border: string }>(({textColor, backgroundColor, border}) => ({
+  backgroundColor: backgroundColor,
+  color: textColor,
+  fontSize: "12px",
+  fontWeight: 700,
+  padding: "12px 45px",
+  display: "inline-block",
+  textAlign: "center",
+  whiteSpace: "nowrap",
+  cursor: "pointer",
+  textTransform: "none",
+  border: border,
+  textDecoration: "none",
+}));
+
+
+interface Props {
+  text: string;
+  color: string;
+  backgroundColor: string;
+  border: string;
+}
+
+const TryFreeButton: React.FC<Props> = ({text, color, backgroundColor, border}) => {
+  return (
+    <StyledButton textColor={color} backgroundColor={backgroundColor} border={border}>
+      {text}
+    </StyledButton>
+  );
+};
+
+export default TryFreeButton;
