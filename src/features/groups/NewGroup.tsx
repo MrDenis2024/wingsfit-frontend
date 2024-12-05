86408da83cf26a8b74fb4dfe5180dbd10165a47d
Konstantin <115252606+koog7@users.noreply.github.com>
2024-12-05 16:39:10 +0600

    Изменена форма

--- src/features/groups/NewGroup.tsx
+++ src/features/groups/NewGroup.tsx
@@ -1,24 +1,34 @@
-import { useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../app/hooks.ts";
-import { selectGroupCreate } from "./groupsSlice.ts";
+import {selectGroupCreate, selectGroupError} from "./groupsSlice.ts";
 import { GroupMutation } from "../../types/groupTypes.ts";
 import { createGroup } from "./groupsThunk.ts";
 import { Typography } from "@mui/material";
 import GroupForm from "./components/GroupForm.tsx";
+import {useEffect} from "react";
+import {toast} from "react-toastify";
+
 
 const NewGroup = () => {
-  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const isCreating = useAppSelector(selectGroupCreate);
-
+  const error = useAppSelector(selectGroupError)
   const onFormSubmit = async (groupMutation: GroupMutation) => {
     try {
       await dispatch(createGroup(groupMutation));
-      navigate("/");
+      if(!error?.error){
+        toast.success("Группа успешно создана!");
+      }
     } catch (error) {
       console.error("Course creation error", error);
     }
   };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error.error);
+    }
+  }, [error]);
+
   return (
     <>
       <Typography variant="h4" sx={{ mb: 2 }}>
--- src/features/groups/components/GroupForm.tsx
+++ src/features/groups/components/GroupForm.tsx
@@ -6,9 +6,19 @@ import {
 } from "../../courses/coursesSlice.ts";
 import { GroupMutation } from "../../../types/groupTypes.ts";
 import { fetchCourses } from "../../courses/coursesThunks.ts";
-import { CircularProgress, Grid2, MenuItem, TextField } from "@mui/material";
+import {
+  CircularProgress,
+  FormControl,
+  Grid2,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField
+} from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
 import SaveIcon from "@mui/icons-material/Save";
+import {selectUser} from "../../users/userSlice.ts";
 
 interface Props {
   onSubmit: (course: GroupMutation) => void;
@@ -19,19 +29,20 @@ const GroupForm: React.FC<Props> = ({ onSubmit, isLoading }) => {
   const dispatch = useAppDispatch();
   const courses = useAppSelector(selectCourses);
   const coursesFetching = useAppSelector(selectCoursesFetching);
+  const user = useAppSelector(selectUser);
   const [state, setState] = useState<GroupMutation>({
     title: "",
     course: "",
     startTime: "",
+    trainingLevel:"junior",
   });
 
   useEffect(() => {
-    dispatch(fetchCourses());
+    dispatch(fetchCourses(user?._id));
   }, [dispatch]);
 
   const submitFormHandler = (event: React.FormEvent) => {
     event.preventDefault();
-    console.log(state);
     onSubmit({ ...state });
   };
 
@@ -44,6 +55,14 @@ const GroupForm: React.FC<Props> = ({ onSubmit, isLoading }) => {
       [name]: value,
     }));
   };
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setState((prevState) => ({
+      ...prevState,
+      trainingLevel: event.target.value,
+    }));
+  };
+
   return (
     <Grid2
       container
@@ -91,12 +110,29 @@ const GroupForm: React.FC<Props> = ({ onSubmit, isLoading }) => {
         <TextField
           required
           type="Time"
-          label="Start Time"
           id="startTime"
           name="startTime"
           value={state.startTime}
           onChange={inputChangeHandler}
         />
+      </Grid2>
+      <Grid2>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+              required
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={state.trainingLevel}
+              label="Age"
+              variant="outlined"
+              onChange={handleChange}
+          >
+            <MenuItem value={'junior'}>Junior</MenuItem>
+            <MenuItem value={'middle'}>Middle</MenuItem>
+            <MenuItem value={'advanced'}>Advanced</MenuItem>
+          </Select>
+        </FormControl>
       </Grid2>
       <Grid2>
         <LoadingButton
@@ -106,7 +142,7 @@ const GroupForm: React.FC<Props> = ({ onSubmit, isLoading }) => {
           startIcon={<SaveIcon />}
           variant="contained"
         >
-          <span>Save</span>
+          <span>Create</span>
         </LoadingButton>
       </Grid2>
     </Grid2>
