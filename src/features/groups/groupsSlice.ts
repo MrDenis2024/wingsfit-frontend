770809c07b9674382271f20543ee5317f2f2abd3
Ivan Limarev <ivantaketo@gmail.com>
2024-12-06 14:40:15 +0600

    groupsSlice.ts, groupsThunk.ts, groupTypes.ts Создание типа Group, настройка GET запросов на получение всех групп тренера и групп курса.

--- src/features/groups/groupsSlice.ts
+++ src/features/groups/groupsSlice.ts
@@ -1,13 +1,20 @@
 import { GlobalError } from "../../types/userTypes.ts";
 import { createSlice } from "@reduxjs/toolkit";
-import { createGroup } from "./groupsThunk.ts";
+import {createGroup, fetchAllGroups, fetchCourseGroups} from "./groupsThunk.ts";
+import {IGroup} from "../../types/groupTypes.ts";
 
 export interface GroupsState {
+  groupsData: IGroup[];
+  fetchGroups: boolean;
+  fetchCourseGroups: boolean;
   isCreating: boolean;
   isCreatingError: GlobalError | null;
 }
 
 const initialState: GroupsState = {
+  groupsData: [],
+  fetchGroups: false,
+  fetchCourseGroups: false,
   isCreating: false,
   isCreatingError: null,
 };
@@ -29,8 +36,37 @@ export const groupsSlice = createSlice({
         state.isCreating = false;
         state.isCreatingError = error || null;
       });
+
+    builder
+      .addCase(fetchAllGroups.pending, (state) => {
+        state.fetchGroups = true;
+      })
+      .addCase(fetchAllGroups.fulfilled, (state, {payload: groupsData}) => {
+        state.groupsData = groupsData;
+        state.fetchGroups = false;
+      })
+      .addCase(fetchAllGroups.rejected, (state,) => {
+        state.fetchGroups = false;
+      });
+
+
+    builder
+      .addCase(fetchCourseGroups.pending, (state) => {
+        state.fetchCourseGroups = true;
+      })
+      .addCase(fetchCourseGroups.fulfilled, (state, {payload: groupsData}) => {
+        state.groupsData = groupsData;
+        state.fetchCourseGroups = false;
+      })
+      .addCase(fetchCourseGroups.rejected, (state,) => {
+        state.fetchCourseGroups = false;
+      });
+
   },
   selectors: {
+    selectGroups: (state) => state.groupsData,
+    selectFetchGroups: (state) => state.fetchGroups,
+    selectFetchCourseGroups: (state) => state.fetchCourseGroups,
     selectGroupCreate: (state) => state.isCreating,
     selectGroupError: (state) => state.isCreatingError,
   },
@@ -38,4 +74,10 @@ export const groupsSlice = createSlice({
 
 export const groupsReducer = groupsSlice.reducer;
 
-export const { selectGroupCreate, selectGroupError } = groupsSlice.selectors;
+export const {
+  selectGroupCreate,
+  selectGroupError,
+  selectFetchGroups,
+  selectFetchCourseGroups,
+  selectGroups
+} = groupsSlice.selectors;
--- src/types/groupTypes.ts
+++ src/types/groupTypes.ts
@@ -3,3 +3,20 @@ export interface GroupMutation {
   course: string;
   startTime: string;
 }
+
+export interface IGroup {
+  _id: string;
+  title: string;
+  course: {
+    _id: string;
+    title: string;
+  };
+  clients: {
+    _id: string,
+    firstName: string,
+    lastName: string,
+  }[];
+  clientsLimit: number;
+  startTime: string;
+  trainingLevel: string;
+}
