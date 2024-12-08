4fcfad7984985a60bbff1f5e53ab0e53974206dd
alyonaVet <a.v.alenka2015@gmail.com>
2024-12-08 18:27:15 +0600

    вынесены стили CourseCard для бопее гибкой верстки;

--- src/features/courses/components/CourseCards.tsx
+++ src/features/courses/components/CourseCards.tsx
@@ -5,6 +5,15 @@ import { CircularProgress, Grid2 } from "@mui/material";
 import CourseCard from "./CourseCard.tsx";
 import { selectCoursesFetching } from "../coursesSlice.ts";
 
+const cardBoxSx = {
+  width: {
+    xs: "100%",
+    sm: "50%",
+    md: "50%",
+    lg: "33%",
+  }
+};
+
 interface Props {
   courses: ICourse[];
 }
@@ -14,12 +23,15 @@ const CourseCards: React.FC<Props> = ({ courses }) => {
   return (
     <Grid2
       container
-      spacing={3}
       justifyContent="space-around"
       alignItems="center"
     >
       {!isLoading ? (
-        courses.map((course) => <CourseCard key={course._id} course={course} />)
+        courses.map((course) =>
+          <Grid2 key={course._id} sx={cardBoxSx}>
+          <CourseCard course={course} />
+          </Grid2>
+        )
       ) : (
         <CircularProgress />
       )}
