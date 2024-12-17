3168fac5b69e898a8965980c0fdf8c7d46868a8c
Toktobekov Aidar <aidar.omurbekov05@gmail.com>
2024-12-17 15:03:55 +0600

    Страница welcomePage, footer и header отредактированы, код отформатирован, страница адаптируется под любое устройство!

--- src/UI/Layout/Layout.tsx
+++ src/UI/Layout/Layout.tsx
@@ -3,20 +3,23 @@ import { Container } from "@mui/material";
 import { Location, useLocation } from "react-router-dom";
 import AppToolbar from "../AppToolbar/AppToolbar";
 import Footer from "../Footer/Footer.tsx";
+import { useAppSelector } from "../../app/hooks.ts";
+import { selectUser } from "../../features/users/userSlice.ts";
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const user = useAppSelector(selectUser);
   const location: Location = useLocation();
 
   const onExcludedPage =
     location.pathname.includes("/login") ||
     location.pathname.includes("/register") ||
     location.pathname.includes("/fill-profile");
+  const onExcludedMainPage = location.pathname === "/";
+  const showHeader = onExcludedMainPage && !user;
 
   return (
     <>
-      <header>
-        <AppToolbar />{" "}
-      </header>
+      <header>{showHeader || onExcludedPage ? <></> : <AppToolbar />}</header>
       <Container
         maxWidth={false}
         component="main"
