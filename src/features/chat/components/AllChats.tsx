fbb1cb229b3f6ad65e376c6dbf79423dc77e6210
alyonaVet <a.v.alenka2015@gmail.com>
2024-12-18 15:22:30 +0600

    изменен компонент AllChats для отображения групповых и личных чатов;

--- src/features/chat/components/AllChats.tsx
+++ src/features/chat/components/AllChats.tsx
@@ -1,30 +1,58 @@
-import { Typography } from "@mui/material";
+import {Divider, Typography} from "@mui/material";
 import React from "react";
-import ChatsList from "./ChatsList.tsx";
 import Grid from "@mui/material/Grid2";
-import { OneChat } from "../../../types/chatTypes.ts";
+import {OneChat} from "../../../types/chatTypes.ts";
+import ChatsList from "./ChatsList.tsx";
 
 interface Props {
   chats: OneChat[];
   setSelectedChatId: (chatId: string) => void;
   selectedChatId: string | null;
+  currentUserId?: string;
 }
 
 const AllChats: React.FC<Props> = ({
-  chats,
-  setSelectedChatId,
-  selectedChatId,
-}) => {
+                                     chats,
+                                     setSelectedChatId,
+                                     selectedChatId,
+                                     currentUserId,
+                                   }) => {
+  const groupChats = chats.filter(chat => chat.type === "group");
+  const privateChats = chats.filter(chat => chat.type === "private");
+
   return (
-    <Grid>
-      <Typography variant="h4" textAlign="center">
-        All chats
-      </Typography>
-      <ChatsList
-        chats={chats}
-        setSelectedChatId={setSelectedChatId}
-        selectedChatId={selectedChatId}
-      />
+    <Grid container direction="column" sx={{maxWidth: 300, backgroundColor: "#eeeeee", height: "100%"}}>
+      <Grid sx={{borderBottom: "1px solid #ccc"}}>
+        <Typography variant="h6" sx={{padding: 2}}>
+          Чаты
+        </Typography>
+      </Grid>
+      <Grid>
+        <ChatsList
+          chats={groupChats}
+          setSelectedChatId={setSelectedChatId}
+          selectedChatId={selectedChatId}
+          title="Групповые чаты"
+          renderChatTitle={(chat) => (chat.type === "group" ? chat.title : "")}
+        />
+      </Grid>
+      <Divider/>
+      <Grid>
+        <ChatsList
+          chats={privateChats}
+          setSelectedChatId={setSelectedChatId}
+          selectedChatId={selectedChatId}
+          title="Приватные чаты"
+          renderChatTitle={(chat) => {
+            if (chat.type === "private") {
+              const chatWith =
+                chat.firstPerson._id === currentUserId ? chat.secondPerson : chat.firstPerson;
+              return `${chatWith.firstName} ${chatWith.lastName}`;
+            }
+            return "";
+          }}
+        />
+      </Grid>
     </Grid>
   );
 };
