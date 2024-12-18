ebfe6c045b3a9ba64119330dcc65106e188173de
alyonaVet <a.v.alenka2015@gmail.com>
2024-12-18 15:26:45 +0600

    изменен компонент чата для отображения всех чатов и сообщений; отображение списка всех чатов; использование `useEffect` для отправки действий по извлечению чата;

--- src/features/chat/Chat.tsx
+++ src/features/chat/Chat.tsx
@@ -1,37 +1,42 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import Grid from "@mui/material/Grid2";
 import AllChats from "./components/AllChats.tsx";
 import Messages from "./components/Messages.tsx";
-import { OneChat } from "../../types/chatTypes.ts";
-import { Container } from "@mui/material";
+import {Container} from "@mui/material";
+import {useAppDispatch, useAppSelector} from "../../app/hooks.ts";
+import {selectGroupChats, selectPrivateChats} from "./chatsSlice.ts";
+import {getGroupChats, getPrivateChats} from "./chatsThunks.ts";
+import {selectUser} from "../users/userSlice.ts";
+import {OneChat} from "../../types/chatTypes.ts";
 
 const Chat = () => {
   const [selectedChatId, setSelectedChatId] = useState<string | null>(null);
-  const [chats] = useState<OneChat[]>([
-    {
-      _id: "123",
-      title: "Anton",
-    },
-    {
-      _id: "1232",
-      title: "Группа 1",
-      group: "123123123",
-    },
-  ]);
+  const dispatch = useAppDispatch();
+  const currentUserId = useAppSelector(selectUser);
+  const groupChats = useAppSelector(selectGroupChats);
+  const privateChats = useAppSelector(selectPrivateChats);
+
+  useEffect(() => {
+    dispatch(getGroupChats());
+    dispatch(getPrivateChats());
+  }, [dispatch]);
+
+  const chats: OneChat[] = [...groupChats, ...privateChats];
 
   return (
-    <Container maxWidth="lg" sx={{ my: 5 }}>
+    <Container maxWidth="lg">
       <Grid
         container
         spacing={3}
         justifyContent="space-around"
         sx={{ height: "80vh" }}
       >
-        <Grid sx={{ width: "30%", borderRight: "1px solid #000000" }}>
+        <Grid sx={{ width: "30%" }}>
           <AllChats
             chats={chats}
             setSelectedChatId={setSelectedChatId}
             selectedChatId={selectedChatId}
+            currentUserId={currentUserId?._id}
           />
         </Grid>
         <Grid sx={{ width: "60%" }}>
