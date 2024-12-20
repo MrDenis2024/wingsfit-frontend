adc5fbed9f48a8da7f23d691b684d32d2b27e58d
makaapo <152339540+makaapo@users.noreply.github.com>
2024-12-20 12:20:59 +0600

    WIN-126, Создание верстки для чатов, аналогично макету (#76)

--- src/features/chat/Chat.tsx
+++ src/features/chat/Chat.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
-import Grid from "@mui/material/Grid2";
 import AllChats from "./components/AllChats.tsx";
 import Messages from "./components/Messages.tsx";
-import { Container } from "@mui/material";
+import { Container, Box, IconButton, Drawer } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../../app/hooks.ts";
 import { selectGroupChats, selectPrivateChats } from "./chatsSlice.ts";
 import { getGroupChats, getPrivateChats } from "./chatsThunks.ts";
 import { selectUser } from "../users/userSlice.ts";
 import { OneChat } from "../../types/chatTypes.ts";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Chat = () => {
   const [selectedChatId, setSelectedChatId] = useState<string | null>(null);
+  const [drawerOpen, setDrawerOpen] = useState(false);
   const dispatch = useAppDispatch();
   const currentUserId = useAppSelector(selectUser);
   const groupChats = useAppSelector(selectGroupChats);
@@ -23,26 +24,109 @@ const Chat = () => {
 
   const chats: OneChat[] = [...groupChats, ...privateChats];
 
+  const selectedChat = chats.find((chat) => chat._id === selectedChatId);
+
+  const chatTitle = (() => {
+    if (!selectedChat) return "Чат";
+
+    if (selectedChat.type === "group") {
+      return selectedChat.title || "Групповой чат";
+    }
+
+    const isCurrentUserFirstPerson =
+      selectedChat.firstPerson &&
+      selectedChat.firstPerson._id === currentUserId?._id;
+
+    const firstName = isCurrentUserFirstPerson
+      ? selectedChat.secondPerson?.firstName
+      : selectedChat.firstPerson?.firstName;
+
+    const lastName = isCurrentUserFirstPerson
+      ? selectedChat.secondPerson?.lastName
+      : selectedChat.firstPerson?.lastName;
+
+    return `${firstName || "Неизвестно"} ${lastName || "Неизвестно"}`;
+  })();
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
   return (
-    <Container maxWidth="lg">
-      <Grid
-        container
-        spacing={3}
-        justifyContent="space-around"
-        sx={{ height: "80vh" }}
+    <Container maxWidth="lg" sx={{ height: "100vh", padding: 0 }}>
+      <Box
+        sx={{
+          display: { xs: "block", sm: "none" },
+          position: "relative",
+          zIndex: 2,
+        }}
       >
-        <Grid sx={{ width: "30%" }}>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={toggleDrawer}
+          sx={{ position: "absolute", top: 42, left: 16, zIndex: 3 }}
+        >
+          <MenuIcon />
+        </IconButton>
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          height: "100%",
+          border: "1px solid #ddd",
+          overflow: "hidden",
+        }}
+      >
+        <Drawer
+          anchor="left"
+          open={drawerOpen}
+          onClose={toggleDrawer}
+          sx={{
+            display: { xs: "block", sm: "none" },
+          }}
+        >
+          <Box
+            sx={{
+              width: 200,
+              height: "100vh",
+            }}
+          >
+            <AllChats
+              chats={chats}
+              setSelectedChatId={setSelectedChatId}
+              selectedChatId={selectedChatId}
+              currentUserId={currentUserId?._id}
+            />
+          </Box>
+        </Drawer>
+        <Box
+          sx={{
+            width: { sm: "30%", xs: "100%" },
+            borderRight: "1px solid #ddd",
+            overflowY: "auto",
+            display: { xs: "none", sm: "block" },
+          }}
+        >
           <AllChats
             chats={chats}
             setSelectedChatId={setSelectedChatId}
             selectedChatId={selectedChatId}
             currentUserId={currentUserId?._id}
           />
-        </Grid>
-        <Grid sx={{ width: "60%" }}>
-          <Messages chatId={selectedChatId} />
-        </Grid>
-      </Grid>
+        </Box>
+
+        <Box
+          sx={{
+            width: { sm: "70%", xs: "100%" },
+            overflowY: "auto",
+          }}
+        >
+          <Messages chatId={selectedChatId} chatTitle={chatTitle || "Чат"} />
+        </Box>
+      </Box>
     </Container>
   );
 };
