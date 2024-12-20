c90e6af759e28b1d81368d8fdbe9145683248ffa
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-12-20 21:43:38 +0600

    WIN-140, Визуально изменена регистрация, сделал prettier

--- src/features/trainers/components/TrainerProfileDetails.tsx
+++ src/features/trainers/components/TrainerProfileDetails.tsx
@@ -42,13 +42,13 @@ interface TrainerProfileDetailsProps {
 }
 
 const TrainerProfileDetails: React.FC<TrainerProfileDetailsProps> = ({
-    trainerProfile,
-    courses,
-    isOwner,
-    id,
-    showForm,
-    setShowForm,
-    handleReviewSubmit,
+  trainerProfile,
+  courses,
+  isOwner,
+  id,
+  showForm,
+  setShowForm,
+  handleReviewSubmit,
 }) => {
   const [open, setOpen] = useState(false);
 
@@ -67,40 +67,81 @@ const TrainerProfileDetails: React.FC<TrainerProfileDetailsProps> = ({
   };
 
   const isSmallScreen = useMediaQuery((theme: Theme) =>
-      theme.breakpoints.down("md"),
+    theme.breakpoints.down("md"),
   );
 
   return (
-      <>
-        <Box sx={{ backgroundColor: "#dedfe3", paddingY: "25px" }}>
-          <Container
-              maxWidth="lg"
+    <>
+      <Box sx={{ backgroundColor: "#dedfe3", paddingY: "25px" }}>
+        <Container
+          maxWidth="lg"
+          sx={{
+            my: 5,
+          }}
+        >
+          <Grid container>
+            <Grid
+              size={{ md: 4, sm: 6, xs: 12 }}
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+            >
+              <CardMedia
+                component="img"
+                image={cardImage}
+                alt={`Фото тренера ${trainerProfile?.user.firstName}`}
+                sx={{
+                  width: 220,
+                  height: 220,
+                  borderRadius: "50%",
+                  objectFit: "cover",
+                  margin: "0 auto",
+                }}
+              />
+              {isOwner && (
+                <IconButton
+                  onClick={handleClickOpen}
+                  sx={{
+                    color: "#0288D1",
+                    borderColor: "#0288D1",
+                    "&:hover": {
+                      backgroundColor: "#dff3fc",
+                      borderColor: "#0288D1",
+                    },
+                  }}
+                >
+                  <CameraAltIcon />
+                </IconButton>
+              )}
+            </Grid>
+            <Grid
+              size={{ md: 7, sm: 6, xs: 12 }}
+              justifyContent={{ xs: "center" }}
               sx={{
-                my: 5,
+                px: "15px",
               }}
-          >
-            <Grid container>
-              <Grid
-                  size={{ md: 4, sm: 6, xs: 12 }}
-                  display="flex"
-                  flexDirection="column"
-                  alignItems="center"
+            >
+              <Box
+                sx={{ display: "flex", flexDirection: "column", gap: "10px" }}
               >
-                <CardMedia
-                    component="img"
-                    image={cardImage}
-                    alt={`Фото тренера ${trainerProfile?.user.firstName}`}
-                    sx={{
-                      width: 220,
-                      height: 220,
-                      borderRadius: "50%",
-                      objectFit: "cover",
-                      margin: "0 auto",
-                    }}
-                />
-                {isOwner && (
-                    <IconButton
-                        onClick={handleClickOpen}
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Typography
+                    variant={isSmallScreen ? "h5" : "h4"}
+                    sx={{ fontWeight: "bold" }}
+                  >
+                    {trainerProfile?.user.firstName}{" "}
+                    {trainerProfile?.user.lastName}
+                  </Typography>
+                  {isOwner && (
+                    <Link
+                      to={`/edit-trainer/${id}`}
+                      style={{ textDecoration: "none" }}
+                    >
+                      <IconButton
                         sx={{
                           color: "#0288D1",
                           borderColor: "#0288D1",
@@ -109,212 +150,171 @@ const TrainerProfileDetails: React.FC<TrainerProfileDetailsProps> = ({
                             borderColor: "#0288D1",
                           },
                         }}
-                    >
-                      <CameraAltIcon />
-                    </IconButton>
-                )}
-              </Grid>
-              <Grid
-                  size={{ md: 7, sm: 6, xs: 12 }}
-                  justifyContent={{ xs: "center" }}
-                  sx={{
-                    px: "15px",
-                  }}
-              >
-                <Box
-                    sx={{ display: "flex", flexDirection: "column", gap: "10px" }}
-                >
-                  <Box
-                      display="flex"
-                      justifyContent="space-between"
-                      alignItems="center"
-                  >
-                    <Typography
-                        variant={isSmallScreen ? "h5" : "h4"}
-                        sx={{ fontWeight: "bold" }}
-                    >
-                      {trainerProfile?.user.firstName}{" "}
-                      {trainerProfile?.user.lastName}
-                    </Typography>
-                    {isOwner && (
-                        <Link
-                            to={`/edit-trainer/${id}`}
-                            style={{ textDecoration: "none" }}
-                        >
-                          <IconButton
-                              sx={{
-                                color: "#0288D1",
-                                borderColor: "#0288D1",
-                                "&:hover": {
-                                  backgroundColor: "#dff3fc",
-                                  borderColor: "#0288D1",
-                                },
-                              }}
-                          >
-                            <BorderColorIcon />
-                          </IconButton>
-                        </Link>
-                    )}
-                  </Box>
-                  <Typography
-                      variant="h6"
-                      sx={{ fontWeight: "600", marginBottom: "10px" }}
-                  >
-                    Данные пользователя:
-                  </Typography>
-                  <Typography
-                      variant="body2"
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "5px",
-                        fontSize: "16px",
-                        marginBottom: "5px",
-                      }}
-                  >
-                    <LocalPhoneIcon />
-                    {isSmallScreen ? <></> : <strong>Номер телефона:</strong>}
-                    <span>{trainerProfile?.user.phoneNumber || "N/A"}</span>
-                  </Typography>
-                  <Typography
-                      variant="body2"
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "5px",
-                        fontSize: "16px",
-                        marginBottom: "5px",
-                      }}
-                  >
-                    <CelebrationIcon />
-                    {isSmallScreen ? <></> : <strong>Дата рождения: </strong>}
-                    <span>
-                    {trainerProfile?.user.dateOfBirth.slice(0, 10) || "N/A"}
-                  </span>
-                  </Typography>
-                  <Typography
-                      variant="body2"
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "5px",
-                        fontSize: "16px",
-                        marginBottom: "5px",
-                      }}
-                  >
-                    {trainerProfile?.user.gender === "male" && <MaleIcon />}
-                    {trainerProfile?.user.gender === "female" && <FemaleIcon />}
-                    {trainerProfile?.user.gender === "other" && (
-                        <TransgenderIcon />
-                    )}
-                    {isSmallScreen ? <></> : <strong>Пол:</strong>}{" "}
-                    <span style={{ textTransform: "capitalize" }}>
-                    {trainerProfile?.user.gender || "N/A"}
-                  </span>
-                  </Typography>
-                  <Typography
-                      variant="body1"
-                      sx={{ display: "flex", gap: "10px" }}
-                  >
-                    <FitnessCenterIcon />
-                    {isSmallScreen ? <></> : <strong>Специализация:</strong>}{" "}
-                    {trainerProfile?.specialization || "Специализация не указана"}
-                  </Typography>
-                  <Typography
-                      variant="body1"
-                      sx={{ display: "flex", gap: "10px" }}
-                  >
-                    <AutoAwesomeIcon />
-                    {isSmallScreen ? <> </> : <strong>Опыт: </strong>}
-                    {trainerProfile?.experience || "Опыт не указан"}
-                  </Typography>
-
-                  <TrainerCertificates trainerProfile={trainerProfile} />
-
-                  {isOwner && <NewAddTrainerCertificates />}
-                  {!isOwner && (
-                      <Button
-                          variant="contained"
-                          color="success"
-                          sx={{ width: "fit-content" }}
                       >
-                        Связаться с тренером
-                      </Button>
+                        <BorderColorIcon />
+                      </IconButton>
+                    </Link>
                   )}
                 </Box>
-              </Grid>
-            </Grid>
-          </Container>
-        </Box>
+                <Typography
+                  variant="h6"
+                  sx={{ fontWeight: "600", marginBottom: "10px" }}
+                >
+                  Данные пользователя:
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "5px",
+                    fontSize: "16px",
+                    marginBottom: "5px",
+                  }}
+                >
+                  <LocalPhoneIcon />
+                  {isSmallScreen ? <></> : <strong>Номер телефона:</strong>}
+                  <span>{trainerProfile?.user.phoneNumber || "N/A"}</span>
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "5px",
+                    fontSize: "16px",
+                    marginBottom: "5px",
+                  }}
+                >
+                  <CelebrationIcon />
+                  {isSmallScreen ? <></> : <strong>Дата рождения: </strong>}
+                  <span>
+                    {trainerProfile?.user.dateOfBirth.slice(0, 10) || "N/A"}
+                  </span>
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "5px",
+                    fontSize: "16px",
+                    marginBottom: "5px",
+                  }}
+                >
+                  {trainerProfile?.user.gender === "male" && <MaleIcon />}
+                  {trainerProfile?.user.gender === "female" && <FemaleIcon />}
+                  {trainerProfile?.user.gender === "other" && (
+                    <TransgenderIcon />
+                  )}
+                  {isSmallScreen ? <></> : <strong>Пол:</strong>}{" "}
+                  <span style={{ textTransform: "capitalize" }}>
+                    {trainerProfile?.user.gender || "N/A"}
+                  </span>
+                </Typography>
+                <Typography
+                  variant="body1"
+                  sx={{ display: "flex", gap: "10px" }}
+                >
+                  <FitnessCenterIcon />
+                  {isSmallScreen ? <></> : <strong>Специализация:</strong>}{" "}
+                  {trainerProfile?.specialization || "Специализация не указана"}
+                </Typography>
+                <Typography
+                  variant="body1"
+                  sx={{ display: "flex", gap: "10px" }}
+                >
+                  <AutoAwesomeIcon />
+                  {isSmallScreen ? <> </> : <strong>Опыт: </strong>}
+                  {trainerProfile?.experience || "Опыт не указан"}
+                </Typography>
 
-        <Box sx={{ backgroundColor: "#e3f3fc", paddingY: "20px" }}>
-          <Container maxWidth="lg">
-            <Typography
-                variant="body1"
-                sx={{ fontSize: "16px", lineHeight: "1.6" }}
-            >
-              {trainerProfile?.description || "Описание не заполнено"}
-            </Typography>
-          </Container>
-        </Box>
+                <TrainerCertificates trainerProfile={trainerProfile} />
 
-        <Container maxWidth="lg" sx={{ py: 5 }}>
-          <Grid container direction="column">
-            <Grid size={12}>
-              <Typography variant="h5" sx={{ fontWeight: "bold", mb: 5 }}>
-                Доступные курсы
-              </Typography>
-              <CourseCards courses={courses} />
+                {isOwner && <NewAddTrainerCertificates />}
+                {!isOwner && (
+                  <Button
+                    variant="contained"
+                    color="success"
+                    sx={{ width: "fit-content" }}
+                  >
+                    Связаться с тренером
+                  </Button>
+                )}
+              </Box>
             </Grid>
-            <Grid size={12}>
-              <Typography variant="h5" sx={{ fontWeight: "bold", my: 3 }}>
-                Отзывы
-              </Typography>
-            </Grid>
-            <Grid size={12}>
-              <RatingAndReviews id={id} />
-            </Grid>
-
-            <Box sx={{ marginTop: "40px" }}>
-              {!isOwner && (
-                  <>
-                    <Box sx={{ marginTop: "20px", textAlign: "center" }}>
-                      <Button
-                          variant="outlined"
-                          onClick={() => setShowForm(!showForm)}
-                          sx={{
-                            color: "black",
-                            borderColor: "black",
-                            borderRadius: "7px",
-                          }}
-                      >
-                        {showForm ? "Закрыть" : "Оставить отзыв"}
-                      </Button>
-                    </Box>
-                    {showForm && (
-                        <Box
-                            sx={{
-                              marginTop: "20px",
-                              display: "flex",
-                              flexDirection: "column",
-                              alignItems: "center",
-                            }}
-                        >
-                          <ReviewFormBlock onSubmit={handleReviewSubmit} />
-                        </Box>
-                    )}
-                  </>
-              )}
-            </Box>
           </Grid>
-          <AvatarUploader
-              trainerProfile={trainerProfile}
-              clientProfile={null}
-              open={open}
-              onClose={handleClose}
-          />
         </Container>
-      </>
+      </Box>
+
+      <Box sx={{ backgroundColor: "#e3f3fc", paddingY: "20px" }}>
+        <Container maxWidth="lg">
+          <Typography
+            variant="body1"
+            sx={{ fontSize: "16px", lineHeight: "1.6" }}
+          >
+            {trainerProfile?.description || "Описание не заполнено"}
+          </Typography>
+        </Container>
+      </Box>
+
+      <Container maxWidth="lg" sx={{ py: 5 }}>
+        <Grid container direction="column">
+          <Grid size={12}>
+            <Typography variant="h5" sx={{ fontWeight: "bold", mb: 5 }}>
+              Доступные курсы
+            </Typography>
+            <CourseCards courses={courses} />
+          </Grid>
+          <Grid size={12}>
+            <Typography variant="h5" sx={{ fontWeight: "bold", my: 3 }}>
+              Отзывы
+            </Typography>
+          </Grid>
+          <Grid size={12}>
+            <RatingAndReviews id={id} />
+          </Grid>
+
+          <Box sx={{ marginTop: "40px" }}>
+            {!isOwner && (
+              <>
+                <Box sx={{ marginTop: "20px", textAlign: "center" }}>
+                  <Button
+                    variant="outlined"
+                    onClick={() => setShowForm(!showForm)}
+                    sx={{
+                      color: "black",
+                      borderColor: "black",
+                      borderRadius: "7px",
+                    }}
+                  >
+                    {showForm ? "Закрыть" : "Оставить отзыв"}
+                  </Button>
+                </Box>
+                {showForm && (
+                  <Box
+                    sx={{
+                      marginTop: "20px",
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                    }}
+                  >
+                    <ReviewFormBlock onSubmit={handleReviewSubmit} />
+                  </Box>
+                )}
+              </>
+            )}
+          </Box>
+        </Grid>
+        <AvatarUploader
+          trainerProfile={trainerProfile}
+          clientProfile={null}
+          open={open}
+          onClose={handleClose}
+        />
+      </Container>
+    </>
   );
 };
 
--- src/features/users/components/RegisterPreview.tsx
+++ src/features/users/components/RegisterPreview.tsx
@@ -3,7 +3,7 @@ import { TrainerProfileMutation } from "../../../types/trainerTypes.ts";
 import { ClientProfileMutation } from "../../../types/clientTypes.ts";
 import React from "react";
 import Grid from "@mui/material/Grid2";
-import { Typography, Paper } from "@mui/material";
+import { Typography, Paper, Divider } from "@mui/material";
 import { useAppSelector } from "../../../app/hooks.ts";
 import { selectCourseTypes } from "../../CourseTypes/CourseTypesSlice.ts";
 import { findCourseTypes } from "../../../constants.ts";
@@ -39,10 +39,7 @@ const RegisterPreview: React.FC<Props> = ({
       sx={{
         my: 5,
         mx: 1,
-        mt: {
-          xs: "30px",
-          sm: "150px",
-        },
+        pb: 8,
         maxWidth: "600px",
         width: "100%",
         marginLeft: {
@@ -59,21 +56,21 @@ const RegisterPreview: React.FC<Props> = ({
       <Paper
         sx={{
           padding: 3,
-          backgroundColor: "#f5f5f5",
-          borderRadius: "8px",
-          opacity: 0.8,
+          backgroundColor: "rgba(51, 51, 51, 0.8)",
+          borderRadius: "15px",
+          color: "white",
         }}
       >
         <Grid>
           <Typography
-            variant="h6"
+            align="center"
+            variant="h5"
             sx={{
               fontWeight: "bold",
-              textTransform: "uppercase",
-              color: "#444",
+              mb: 2,
             }}
           >
-            Обязательная информация
+            Обязательная информация:
           </Typography>
         </Grid>
 
@@ -87,7 +84,7 @@ const RegisterPreview: React.FC<Props> = ({
                     <span
                       style={{
                         fontWeight: "bold",
-                        textTransform: "uppercase",
+                        textTransform: "capitalize",
                       }}
                     >
                       {key}:
@@ -100,7 +97,7 @@ const RegisterPreview: React.FC<Props> = ({
                     <span
                       style={{
                         fontWeight: "bold",
-                        textTransform: "uppercase",
+                        textTransform: "capitalize",
                       }}
                     >
                       {key}:
@@ -112,16 +109,16 @@ const RegisterPreview: React.FC<Props> = ({
             );
           })}
         </Grid>
-
+        <Divider sx={{ backgroundColor: "white" }} />
         {role === "trainer" && (
           <>
             <Grid>
               <Typography
-                variant="h6"
+                align="center"
+                variant="h5"
                 sx={{
                   fontWeight: "bold",
-                  textTransform: "uppercase",
-                  color: "#444",
+                  mb: 2,
                 }}
               >
                 Дополнительная информация о тренере
@@ -137,7 +134,7 @@ const RegisterPreview: React.FC<Props> = ({
                         <span
                           style={{
                             fontWeight: "bold",
-                            textTransform: "uppercase",
+                            textTransform: "capitalize",
                           }}
                         >
                           {key} :{" "}
@@ -152,7 +149,7 @@ const RegisterPreview: React.FC<Props> = ({
                             <span
                               style={{
                                 fontWeight: "bold",
-                                textTransform: "uppercase",
+                                textTransform: "capitalize",
                               }}
                             >
                               {key} :{" "}
@@ -180,14 +177,14 @@ const RegisterPreview: React.FC<Props> = ({
           <>
             <Grid>
               <Typography
-                variant="h6"
+                variant="h5"
+                align="center"
                 sx={{
                   fontWeight: "bold",
-                  textTransform: "uppercase",
-                  color: "#444",
+                  mb: 2,
                 }}
               >
-                Информация о клиенте
+                Информация о клиенте:
               </Typography>
             </Grid>
             <Grid>
@@ -200,7 +197,7 @@ const RegisterPreview: React.FC<Props> = ({
                         <span
                           style={{
                             fontWeight: "bold",
-                            textTransform: "uppercase",
+                            textTransform: "capitalize",
                           }}
                         >
                           {key}:
@@ -215,7 +212,7 @@ const RegisterPreview: React.FC<Props> = ({
                             <span
                               style={{
                                 fontWeight: "bold",
-                                textTransform: "uppercase",
+                                textTransform: "capitalize",
                               }}
                             >
                               {key} :{" "}
