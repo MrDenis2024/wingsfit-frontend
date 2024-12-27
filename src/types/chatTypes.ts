8fe8259b81aab3050ec083a7713ffc672ce103c9
alyonaVet <a.v.alenka2015@gmail.com>
2024-12-27 14:57:37 +0600

    были измененны типы; были удалены неиспользованные;

--- src/types/chatTypes.ts
+++ src/types/chatTypes.ts
@@ -24,86 +24,15 @@ export interface PrivateChat {
   };
 }
 
-export interface FetchMessagesParams {
+export interface Message {
+  _id: string;
   chatId: string;
-  page: number;
-  limit: number;
-}
-
-export interface PrivateChatMessage {
-  _id: string;
-  privateChat: string;
   author: {
     _id: string;
     firstName: string;
     lastName: string;
+    avatar?: string;
   };
   message: string;
   createdAt: string;
-  isRead: {
-    _id: string;
-    user: string;
-    read: boolean;
-  };
 }
-
-export interface GroupChatMessage {
-  _id: string;
-  groupChat: string;
-  author: {
-    _id: string;
-    firstName: string;
-    lastName: string;
-  };
-  message: string;
-  createdAt: string;
-  isRead: {
-    _id: string;
-    user: string;
-    read: boolean;
-  };
-}
-
-export interface ChatMessages {
-  [chatId: string]: {
-    messages: (GroupChatMessage | PrivateChatMessage)[];
-    hasMore: boolean;
-    error: string | null;
-  };
-}
-
-export interface SuccessLoginIncomingMessage {
-  type: "LOGIN_SUCCESS";
-  payload: { userName: string; userId: string };
-}
-
-export interface LatestMessagesIncomingMessage {
-  type: "GET_LAST";
-  payload: {
-    latestMessages: (GroupChatMessage | PrivateChatMessage)[];
-  };
-}
-
-export interface SendingMessageIncomingMessage {
-  type: "SEND_MESSAGE";
-  payload: {
-    message: string;
-  };
-}
-
-export interface NewMessageIncomingMessage {
-  type: "NEW_MESSAGE";
-  payload: GroupChatMessage | PrivateChatMessage;
-}
-
-export interface ErrorIncomingMessage {
-  type: "ERROR";
-  payload: string;
-}
-
-export type IncomingMessage =
-  | SuccessLoginIncomingMessage
-  | LatestMessagesIncomingMessage
-  | SendingMessageIncomingMessage
-  | NewMessageIncomingMessage
-  | ErrorIncomingMessage;
