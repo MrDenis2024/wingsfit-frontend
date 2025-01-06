7290fc7a55dcdc402bdc9b1eecc377861a1eb308
alyonaVet <a.v.alenka2015@gmail.com>
2025-01-06 12:56:23 +0600

    добавлена обработка состояний createPrivateChat в chatsSlice;

--- src/features/chat/chatsSlice.ts
+++ src/features/chat/chatsSlice.ts
@@ -1,6 +1,10 @@
 import { GroupChat, PrivateChat } from "../../types/chatTypes.ts";
 import { createSlice } from "@reduxjs/toolkit";
-import { getGroupChats, getPrivateChats } from "./chatsThunks.ts";
+import {
+  createPrivateChat,
+  getGroupChats,
+  getPrivateChats,
+} from "./chatsThunks.ts";
 
 interface ChatsState {
   groupChats: GroupChat[];
@@ -43,6 +47,16 @@ export const chatsSlice = createSlice({
         },
       )
       .addCase(getPrivateChats.rejected, (state) => {
+        state.privateChatsFetching = false;
+      });
+    builder
+      .addCase(createPrivateChat.pending, (state) => {
+        state.privateChatsFetching = true;
+      })
+      .addCase(createPrivateChat.fulfilled, (state, { payload: newChat }) => {
+        state.privateChats.push(newChat);
+      })
+      .addCase(createPrivateChat.rejected, (state) => {
         state.privateChatsFetching = false;
       });
   },
