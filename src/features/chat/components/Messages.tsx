1d5ecf3c1c1362e56ee422c3d95a26d8f390025c
alyonaVet <a.v.alenka2015@gmail.com>
2025-01-08 21:06:20 +0600

    добавлен тип IncomingMessage для типа сообщений; обновлен sendMessage для полей privateChat и groupChat на основе chatType; добавлено свойство isRead для каждого нового сообщения со значениями по умолчанию;

--- src/features/chat/components/Messages.tsx
+++ src/features/chat/components/Messages.tsx
@@ -5,7 +5,7 @@ import MessagesList from "./MessagesList.tsx";
 import ChatForm from "./ChatForm.tsx";
 import {useAppSelector} from "../../../app/hooks.ts";
 import {selectUser} from "../../users/userSlice.ts";
-import {Message} from "../../../types/chatTypes.ts";
+import {IncomingMessage, Message} from "../../../types/chatTypes.ts";
 import {apiURL, wsApiURL} from "../../../constants.ts";
 
 interface MessagesProps {
@@ -55,7 +55,7 @@ const Messages: React.FC<MessagesProps> = ({ chatId, chatType, chatTitle }) => {
         };
 
         ws.current!.onmessage = (event) => {
-          const data = JSON.parse(event.data);
+          const data: IncomingMessage = JSON.parse(event.data);
           if (
             (data.type === "NEW_MESSAGE" &&
               (data.payload.privateChat === chatId || data.payload.groupChat === chatId))
@@ -94,7 +94,8 @@ const Messages: React.FC<MessagesProps> = ({ chatId, chatType, chatTitle }) => {
       ...prevMessages,
       {
         _id: Date.now().toString(),
-        chatId: chatId || "",
+        privateChat: chatType === "private" ? chatId || undefined : undefined,
+        groupChat: chatType === "group" ? chatId || undefined : undefined,
         author: {
           _id: user?._id || "",
           firstName: user?.firstName || "",
@@ -103,6 +104,10 @@ const Messages: React.FC<MessagesProps> = ({ chatId, chatType, chatTitle }) => {
         },
         message,
         createdAt: new Date().toISOString(),
+        isRead: {
+          user: user?._id || "",
+          read: false,
+        },
       },
     ]);
   };
