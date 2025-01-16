001cee9b85aa2629752e91c95bead5469ddf9487
Khrunev Denis <deniskhrunev@gmail.com>
2025-01-16 20:53:24 +0600

    WIN-175 Создана структура по изменению пароля пользователя

--- src/features/admin/admins/components/AdminPanelItem.tsx
+++ src/features/admin/admins/components/AdminPanelItem.tsx
@@ -26,6 +26,8 @@ import { selectDeleteGroupLoading } from "../../../groups/groupsSlice.ts";
 import CustomConfirmDialog from "../../../../UI/CustomConfirmDialog/CustomConfirmDialog.tsx";
 import { selectDeleteCourseLoading } from "../../../courses/coursesSlice.ts";
 import { deleteCourse, fetchCourses } from "../../../courses/coursesThunks.ts";
+import Modal from "../../../../UI/Modal/Modal.tsx";
+import ChangePassword from "../../../users/components/ChangePassword.tsx";
 
 interface Props {
   users: IUser[];
@@ -49,6 +51,7 @@ const AdminPanelItem: React.FC<Props> = ({
   const deleteGroupLoading = useAppSelector(selectDeleteGroupLoading);
   const courseDeleteLoading = useAppSelector(selectDeleteCourseLoading);
   const [confirmOpen, setConfirmOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [itemToDelete, setItemToDelete] = useState<{
     type: "group" | "course";
     id: string;
@@ -419,6 +422,13 @@ const AdminPanelItem: React.FC<Props> = ({
           <Button color="inherit" onClick={() => setActiveTab("courses")}>
             Курсы
           </Button>
+          <Button
+            onClick={() => setModalOpen(true)}
+            color="inherit"
+            sx={{ marginLeft: "auto" }}
+          >
+            Сменить пароль
+          </Button>
         </Toolbar>
       </AppBar>
       {renderContent()}
@@ -431,6 +441,13 @@ const AdminPanelItem: React.FC<Props> = ({
         onConfirm={handleDelete}
         onCancel={() => setConfirmOpen(false)}
       />
+      <Modal
+        title={"Сменить пароль"}
+        onClose={() => setModalOpen(false)}
+        show={modalOpen}
+      >
+        <ChangePassword onClose={() => setModalOpen(false)} />
+      </Modal>
     </>
   );
 };
